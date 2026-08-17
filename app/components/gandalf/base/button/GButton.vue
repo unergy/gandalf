3683a394f01414d7652c8356339238c2e1181661
imlargo <jclargob@unal.edu.co>
2026-08-17 16:32:09 -0500

    feat(components): create GButton component with props and template

---
+++ app/components/gandalf/base/button/GButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { PrimitiveProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import type { GandalfButtonVariants } from './index'
+import { reactiveOmit } from '@vueuse/core'
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<
+  PrimitiveProps & {
+    variant?: GandalfButtonVariants['variant']
+    size?: GandalfButtonVariants['size']
+    class?: HTMLAttributes['class']
+  }
+>()
+
+const delegatedProps = reactiveOmit(props, 'class')
+</script>
+
+<template>
+  <Button v-bind="delegatedProps" :class="cn(props.class)">
+    <slot />
+  </Button>
+</template>
